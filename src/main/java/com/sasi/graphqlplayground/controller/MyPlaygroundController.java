60276a2a3385dce5029e130a1ea477b6671deccf
Sasikanth.Kesani <Sasikanth.Kesani@fda.hhs.gov>
2023-03-28 13:32:04 -0400

    adding controller get method

--- src/main/java/com/sasi/graphqlplayground/controller/MyPlaygroundController.java
+++ src/main/java/com/sasi/graphqlplayground/controller/MyPlaygroundController.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import graphql.kickstart.playground.boot.PlaygroundController;
 import graphql.kickstart.playground.boot.PlaygroundPropertiesConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,11 +15,21 @@ import java.util.Map;
 public class MyPlaygroundController extends PlaygroundController {
 
     private static final String _CSRF = "_csrf";
+    private PlaygroundPropertiesConfiguration propertiesConfiguration;
+    private ObjectMapper objectMapper;
 
     public MyPlaygroundController(PlaygroundPropertiesConfiguration propertiesConfiguration, ObjectMapper objectMapper) {
         super(propertiesConfiguration, objectMapper);
+        System.out.println("in the contoller MyPlaygroundController");
+    }
+
+
+    @GetMapping("playground")
+    public String playground() {
+        System.out.println("in the playground MyPlaygroundController");
         Map<String, String> headers = new HashMap<>();
         headers.put("Authorization", "Bearer testsjhajsjhabdjbjjjbn");
         propertiesConfiguration.getPlayground().setHeaders(headers);
+        return "playground1";
     }
 }
